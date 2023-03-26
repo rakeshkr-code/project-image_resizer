c8de0b64c77683df1b52cb57cc3664a4f2a893c8
Rakesh <mandalrakesh628@gmail.com>
2023-03-26 08:23:29 +0530

    width and height in cm

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const downloadLink = document.getElementById('downloadLink');
 const imagePreview = document.getElementById('imagePreview');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+const DPI = 96; // pixels per inch
 
 function resizeImage() {
 	// Get the uploaded image file
@@ -23,19 +24,27 @@ function resizeImage() {
 	const img = new Image();
 	img.onload = function() {
 		// Set the canvas dimensions to the desired size
-		canvas.width = widthInput.value;
-		canvas.height = heightInput.value;
+		// canvas.width = widthInput.value;
+		// canvas.height = heightInput.value;
+
+		// Convert cm to pixels based on DPI
+		const widthInPx = widthInput.value * DPI / 2.54;
+		const heightInPx = heightInput.value * DPI / 2.54;
+
+		// Set the canvas dimensions to the desired size
+		canvas.width = widthInPx;
+		canvas.height = heightInPx;
 
 		// Draw the image onto the canvas at the new size
-		ctx.drawImage(img, 0, 0, widthInput.value, heightInput.value);
+		ctx.drawImage(img, 0, 0, widthInPx, heightInPx);
 
 		// Export the resized image as a data URL and update the preview
-		const resizedImage = canvas.toDataURL('image/jpeg', 0.8);
+		const resizedImage = canvas.toDataURL('image/jpg', 0.9);
 		imagePreview.src = resizedImage;
 
 		// Update the download link with the data URL of the resized image
 		downloadLink.href = resizedImage;
-		downloadLink.download = 'resized.jpeg';
+		downloadLink.download = 'resized.jpg';
 		downloadLink.innerHTML = 'Download';
 	}
 	img.src = URL.createObjectURL(file);
